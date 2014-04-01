6fbdf6b157ac4fd0ac22a7fc97dff50558870f0c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-04-01 11:07:04 -0400

    Sample producer for dumping large amounts of random messages into rabbit

--- sstelfox/producer.rb
+++ sstelfox/producer.rb
@@ -1,13 +1,41 @@
 
 require 'bunny'
+require 'json'
+require 'securerandom'
 
-connection = Bunny.new(ENV['AMQP_URL'] || "amqp://guest:guest@127.0.0.1:5672")
+connection = Bunny.new(ENV['AMQP_URI'] || "amqp://guest:guest@127.0.0.1:5672")
 connection.start
 
 channel = connection.channel
-exchange = channel.direct('test.workers')
+exchange = channel.direct('pwnie.cloud', :durable => true)
 
-def message_generator(size)
-
+def message_generator(byte_size)
+  JSON.generate({size: byte_size, time: Time.now.to_f, content: SecureRandom.hex(byte_size / 2)})
 end
+
+stats = {}
+stats[:start_time] = Time.now.to_f
+stats[:duration] = 60
+stats[:message_size] = 256
+stats[:counts] = Hash.new(0)
+
+while Time.now.to_f < (stats[:start_time] + stats[:duration])
+  exchange.publish(message_generator(stats[:message_size]), :routing_key => 'test.workers')
+  stats[:counts][Time.now.to_i] += 1
+end
+
+stats[:end_time] = Time.now.to_f
+
+stats[:total_time] = stats[:end_time] - stats[:start_time]
+stats[:total_messages] = msgs_generated = stats[:counts].values.inject(&:+)
+
+generation_start_time = Time.now.to_f
+msgs_generated.times { message_generator(stats[:message_size]) }
+generation_end_time = Time.now.to_f
+
+stats[:message_generation_time] = generation_end_time - generation_start_time
+
+puts JSON.pretty_generate(stats)
+
+connection.close
 
