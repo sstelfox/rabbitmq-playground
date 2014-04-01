3996af41754c768263cc3ebb0cb822516aec732b
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-04-01 13:13:30 -0400

    Removed durable flag

--- sstelfox/producer.rb
+++ sstelfox/producer.rb
@@ -7,7 +7,7 @@ connection = Bunny.new(ENV['AMQP_URI'] || "amqp://guest:guest@127.0.0.1:5672")
 connection.start
 
 channel = connection.channel
-exchange = channel.direct('pwnie.cloud', :durable => true)
+exchange = channel.direct('pwnie.cloud')
 queue = channel.queue("noop", :auto_delete => true).bind(exchange, :routing_key => 'test.workers')
 
 def message_generator(byte_size)
